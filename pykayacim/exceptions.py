f9b0829f5a19d3a118734b94ba1361be62f1028e
Hamukichi (Nombiri) <hamukichi-dev@outlook.jp>
2015-09-08 15:27:03 +0900

    Added PyKayacIMMessageError

--- pykayacim/exceptions.py
+++ pykayacim/exceptions.py
@@ -66,3 +66,18 @@ class PyKayacIMCommunicationError(PyKayacIMError):
 
     def __str__(self):
         return self.reason
+
+@future.utils.python_2_unicode_compatible
+class PyKayacIMMessageError(PyKayacIMError):
+    """Raised when there is something wrong with the message to be sent.
+    
+    :param details: The detailed information about the problem.
+    :type details: str (on Python 3) or unicode (on Python 2)
+    
+    """
+    
+    def __init__(self, details):
+        self.details = details
+    
+    def __str__(self):
+        return self.details
